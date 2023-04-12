2337d48dac03b43d57867861dfe10f1e40ae6047
Alessandro Toschi <aletos@plexagon.it>
2023-04-12 13:57:01 +0200

    [clay] offscreen surface (no impeller)

--- flow/layers/offscreen_surface.h
+++ flow/layers/offscreen_surface.h
@@ -17,6 +17,10 @@ class OffscreenSurface {
   explicit OffscreenSurface(GrDirectContext* surface_context,
                             const SkISize& size);
 
+  OffscreenSurface(int64_t raw_texture,
+                   GrDirectContext* surface_context,
+                   const SkISize& size);
+
   ~OffscreenSurface() = default;
 
   sk_sp<SkData> GetRasterData(bool compressed) const;
@@ -25,8 +29,11 @@ class OffscreenSurface {
 
   bool IsValid() const;
 
+  SkISize size() const { return size_; }
+
  private:
   sk_sp<SkSurface> offscreen_surface_;
+  SkISize size_;
 
   FML_DISALLOW_COPY_AND_ASSIGN(OffscreenSurface);
 };
